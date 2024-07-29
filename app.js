9543ccb6c81fb3bc80cd60f18240f1833845ffb0
Satyam Kumar Navneet <satyamkumarnavneet@Satyams-MacBook-Air.local>
2024-07-30 00:22:44 +0530

    Optimize app.js for better performance

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/todos', todoRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
